b4bad59500af7e618880f013a21f02e0871b4aa8
Emma Hawkins <emma.hawkins@sky.uk>
2025-12-02 16:42:50 +0000

    added a thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import reactLogo from "./assets/react.svg";
+import fine from "./assets/fine.gif";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={fine} className="this_is_fine" alt="This is fne gif" />
         <h1>Hello from Amplify</h1>
+        <p>Learning AWS is going great!!!!!</p>
       </header>
     </div>
   );
